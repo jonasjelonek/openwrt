47e295976e944f348968dcf7e05cb7e453a3918d
Jonas Jelonek <jelonek.jonas@gmail.com>
2025-05-07 12:14:51 +0000

    WIP ms510txm
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/netgear-ms510txm
+  SOC := rtl9313
+  UIMAGE_MAGIC := 0x4E475020
+  DEVICE_VENDOR := Netgear
+  DEVICE_MODEL := MS510TXM
+  DEVICE_PACKAGES := kmod-hwmon-gpiofan kmod-thermal
+  IMAGE_SIZE := 31808k
+endef
+TARGET_DEVICES += netgear-ms510txm
