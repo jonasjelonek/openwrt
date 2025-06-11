d4b5cd075ded0f8efba7ee05753896254a7640cb
Jonas Jelonek <jelonek.jonas@gmail.com>
2025-06-11 22:10:59 +0200

    WIP: imx: use FIT for USB armory
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>

--- target/linux/imx/image/cortexa7.mk
+++ target/linux/imx/image/cortexa7.mk
@@ -2,10 +2,23 @@ DEVICE_VARS += UBOOT
 
 include common.mk
 
+define Build/imx-fit-combined-image
+	$(CP) $(IMAGE_KERNEL) $@.boot/openwrt.itb
+
+        cp $@ $@.fs
+
+        $(SCRIPT_DIR)/gen_image_generic.sh $@ \
+                $(CONFIG_TARGET_KERNEL_PARTSIZE) \
+                $@.boot \
+                $(CONFIG_TARGET_ROOTFS_PARTSIZE) \
+                $@.fs \
+                1024
+endef
+
 define Build/imx-usbarmory-sdcard
 	$(Build/imx-combined-image-prepare)
 
-	$(Build/imx-combined-image)
+	$(Build/imx-fit-combined-image)
 	dd if=$(STAGING_DIR_IMAGE)/$(UBOOT)-u-boot-dtb.imx of=$@ bs=1024 seek=1 conv=notrunc
 
 	$(Build/imx-combined-image-clean)
@@ -49,6 +62,7 @@ define Device/withsecure_usbarmory-mk2
 	kmod-usb-net-cdc-ether kmod-usb-net-cdc-ncm usbgadget
   FILESYSTEMS := squashfs ext4
   SUPPORTED_DEVICES += withsecure,imx6ulz-usbarmory-mk2
+  KERNEL := kernel-bin | lzma | fit lzma $$(DTS_DIR)/$$(DEVICE_DTS).dtb
   IMAGES := combined.bin sysupgrade.bin
   IMAGE/combined.bin := append-rootfs | pad-extra 128k | imx-usbarmory-sdcard
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
